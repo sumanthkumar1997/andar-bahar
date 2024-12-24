3324b4ae8770ae38daf99337474592dd9dc9e40f
Shrikanth <shrikanthbhat30@gmail.com>
2024-12-24 20:53:16 +0530

    add menu popUp in home scene

--- assets/scripts/HomeManager.ts
+++ assets/scripts/HomeManager.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, director, Node, Button, tween, Vec3, Label } from 'cc';
+import { _decorator, Component, director, Node, Button, tween, Vec3, Label, Toggle, ToggleComponent } from 'cc';
 import { GenerateLaunchId } from './Utils/Utils';
 const { ccclass, property } = _decorator;
 
@@ -14,16 +14,36 @@ export class HomeManager extends Component {
     @property(Label)
     LaunchId: Label | null = null;
 
+    @property(Toggle)
+    musicToggle: Toggle|null = null;
+
+    @property(Node)
+    Menu: Node | null = null;
+
     onLoad() {
         let launchId = GenerateLaunchId();
-        this.LaunchId!.string = `Launch ID - ${launchId}`;
+        if(this.LaunchId) {
+            this.LaunchId!.string = `Launch ID - ${launchId}`;
+        }
         if (this.closeButton) {
             this.closeButton.node.on('click', this.closePopUp, this);
         }
     }
 
      start() {
-         
+         localStorage.clear();
+    }
+
+    showMenu() {
+        if(this.Menu) {
+            this.Menu.active = true;
+        }
+    }
+
+    closeMenu() {
+        if(this.Menu) {
+            this.Menu.active = false;
+        }
     }
     
     showPopUp() {    
@@ -41,9 +61,16 @@ export class HomeManager extends Component {
     closePopUp() {
         if (this.popUp) {
             tween(this.popUp)
-                .to(0.5, { scale: new Vec3(0, 0, 0) }, { easing: 'backIn' })
+                .to(0.1, { scale: new Vec3(0, 0, 0) })
                 .call(() => { this.popUp!.active = false; })
                 .start();
+        }
+    }
+
+    audioHandle() {
+        if(this.musicToggle) {
+           const isChecked = this.musicToggle.isChecked;
+           this.musicToggle.isChecked = !isChecked;
         }
     }
 
