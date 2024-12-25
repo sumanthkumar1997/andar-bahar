fda1d46719f309334b675dc42f3d1541ef00b87d
Shrikanth <shrikanthbhat30@gmail.com>
2024-12-25 11:20:28 +0530

    shuffle cards

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, instantiate, Label, Node, Prefab, Sprite, SpriteFrame, tween, Vec3 } from 'cc';
+import { _decorator, Component, instantiate, Label, Node, Prefab, Scheduler, Sprite, SpriteFrame, Tween, tween, Vec3 } from 'cc';
 import { CARD_COUNT } from './constant';
 const { ccclass, property } = _decorator;
 
@@ -25,59 +25,67 @@ export class GameManager extends Component {
     baharPos: Vec3 | null = null;
 
     @property(Label)
-    amount: Label|null = null
+    amount: Label | null = null
 
     @property(Label)
-    winLabel: Label|null = null;    
+    winLabel: Label | null = null;
 
     stackStartX = -150;
     cardsGap = 5.7;
+    shuffleInd = -1;
+    pickedCard = "";
 
-    start () {
+    start() {
         let bet = parseInt(localStorage.getItem("bet") as string);
-        if(bet) {
+        if (bet) {
             let currentAmount = parseInt(this.amount!.string) - bet;
             localStorage.setItem("amount", JSON.stringify(currentAmount));
             this.amount!.string = currentAmount.toString();
 
         }
-        for(let i=0; i<CARD_COUNT; ++i) {
-           let cardNode: any = instantiate(this.CardPrefab);
-           cardNode.getComponent(Sprite).spriteFrame = this.Cards![i];
-           cardNode.setPosition(this.stackStartX + (i * 5.7), 0, 0)
-           this.Stack!.addChild(cardNode);
+        for (let i = 0; i < CARD_COUNT; ++i) {
+            let cardNode: any = instantiate(this.CardPrefab);
+            cardNode.getComponent(Sprite).spriteFrame = this.Cards![i];
+            cardNode.setPosition(this.stackStartX + (i * 5.7), 0, 0)
+            this.Stack!.addChild(cardNode);
         }
 
-        tween(this.Stack?.children[46])
-        .to(0.5, { position: new Vec3(this.Stack?.children[46].position.x, 50, 0)})
-        .to(0.5, { position: new Vec3(this.Openpos!)})
-        .delay(3)
-        .call(() => this.CardAnimation(1))
-        .start()
+        this.shuffleInd = Math.floor(1 + Math.random() * 52);
+        this.pickedCard = this.Stack?.children[this.shuffleInd].getComponent(Sprite)?.spriteFrame?.name.split("_")[2] as string;
+        console.log(this.pickedCard);
+
+
+        tween(this.Stack?.children[this.shuffleInd])
+            .to(0.5, { position: new Vec3(this.Stack?.children[this.shuffleInd].position.x, 50, 0) })
+            .to(0.5, { position: new Vec3(this.Openpos!) })
+            .delay(3)
+            .call(() => {
+                this.CardAnimation(1)
+
+            })
+            .start()
     }
 
     CardAnimation(strtIdx: number) {
         let currentPos = this.andarPos;
-        for(let i=strtIdx; i<(strtIdx + 52); ++i) {
-            if(i != 40) {
+        for (let i = strtIdx; i < (strtIdx + 52); i++) {
+            if (i != this.shuffleInd) {
                 this.scheduleOnce(() => {
-    
                     tween(this.Stack?.children[i])
-                    .to(0.5, { position: currentPos!})
-                    .call(() => {
-                        currentPos!.y === this.andarPos!.y ? currentPos = this.baharPos : currentPos = this.andarPos;
-                        if(this.winLabel) {
-                            if(i == 7) {
-                                this.winLabel.node.active = true;
+                        .to(0.5, { position: currentPos! })
+                        .call(() => {
+                            currentPos!.y === this.andarPos!.y ? currentPos = this.baharPos : currentPos = this.andarPos;
+                            let currentCardName = this.Stack?.children[i].getComponent(Sprite)?.spriteFrame?.name.split("_")[2] as string;
+                            if (currentCardName === this.pickedCard) {
+                                if (this.winLabel) {
+                                    this.winLabel.node.active = true;
+                                    this.unscheduleAllCallbacks();
+                                }
                             }
-                        }
-                    })
-                    .start();
+                        })
+                        .start();
                 }, i)
             }
-
-            if(i === 7)
-                break;
         }
     }
 }
--- assets/scripts/Utils/Utils.ts
+++ assets/scripts/Utils/Utils.ts
@@ -9,3 +9,11 @@ export function GenerateLaunchId() {
       
         return result;
 }
+
+export function shuffle(array: any) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // Generate a random index between 0 and i
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+  }
+  return array;
+}
